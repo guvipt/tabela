6e795360b96c90f7cb090f0a350f19fffa1e1bd4
gustavo <gugasalles.g@gmail.com>
2024-05-15 18:00:34 -0300

    Add files

---
+++ tabala.py
@@ -0,0 +1,23 @@
+elementos = input("digite a sigla de um elemento quimico: ")
+
+elementos = elementos.lower()
+
+if elementos == 'h':
+   print("1")
+   print("Hidrogenio")
+   print("1008")
+
+elif elementos == 'he':
+   print("2")
+   print("helio")
+   print("4003")
+
+elif elementos == 'li':
+   print("3")
+   print("litio")
+   print("6941")
+
+elif elementos == 'be':
+   print("4")
+   print("berilio")
+   print("9012")
